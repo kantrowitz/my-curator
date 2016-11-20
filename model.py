28f88a8e3a8596443155d1f9bd79051b0a30f9b6
Darya Chernikhova <daryachernikhova@nypl.org>
2016-11-20 02:56:11 -0500

    initial model

---
+++ model.py
@@ -0,0 +1,381 @@
+import contextlib
+import json
+import logging # Make sure logging is set up properly.
+
+from nose.tools import set_trace
+
+import os
+
+from sqlalchemy.engine.url import URL
+from sqlalchemy import exc as sa_exc
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import (
+    backref,
+    defer,
+    relationship,
+    sessionmaker,
+)
+from sqlalchemy import (
+    func,
+    or_,
+    MetaData,
+    Table,
+)
+from sqlalchemy.sql import select
+from sqlalchemy.orm import (
+    aliased,
+    backref,
+    defer,
+    contains_eager,
+    joinedload,
+    lazyload,
+)
+from sqlalchemy.orm.exc import (
+    NoResultFound,
+    MultipleResultsFound,
+)
+from sqlalchemy.ext.mutable import (
+    MutableDict,
+)
+from sqlalchemy.ext.associationproxy import (
+    association_proxy,
+)
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.sql.functions import func
+from sqlalchemy.sql.expression import (
+    cast,
+    and_,
+    or_,
+    select,
+    join,
+    literal_column,
+    case,
+    table,
+)
+from sqlalchemy.exc import (
+    IntegrityError
+)
+from sqlalchemy import (
+    create_engine, 
+    Binary,
+    Boolean,
+    Column,
+    Date,
+    DateTime,
+    Enum,
+    Float,
+    ForeignKey,
+    Integer,
+    Index,
+    Numeric,
+    String,
+    Table,
+    Unicode,
+    UniqueConstraint,
+)
+
+
+Base = declarative_base()
+
+
+'''
+Make postgres database with something like:
+CREATE USER amnh_user with password 'test';
+simplified_circulation_dev=# create database my_curator_test;
+simplified_circulation_dev=# grant all privileges on database my_curator_test to amnh_user;
+'''
+
+def production_session():
+    url = Configuration.database_url()
+    if url.startswith('"'):
+        url = url[1:]
+    logging.debug("Database url: %s", url)
+    return SessionManager.session(url)
+
+
+@contextlib.contextmanager
+def temp_config(new_config=None, replacement_classes=None):
+    old_config = Configuration.instance
+    replacement_classes = replacement_classes or [Configuration]
+    if new_config is None:
+        new_config = copy.deepcopy(old_config)
+    try:
+        for c in replacement_classes:
+            c.instance = new_config
+        yield new_config
+    finally:
+        for c in replacement_classes:
+            c.instance = old_config
+
+
+def get_one(db, model, on_multiple='error', **kwargs):
+    q = db.query(model).filter_by(**kwargs)
+    try:
+        return q.one()
+    except MultipleResultsFound, e:
+        if on_multiple == 'error':
+            raise e
+        elif on_multiple == 'interchangeable':
+            # These records are interchangeable so we can use
+            # whichever one we want.
+            #
+            # This may be a sign of a problem somewhere else. A
+            # database-level constraint might be useful.
+            q = q.limit(1)
+            return q.one()
+    except NoResultFound:
+        return None
+
+
+def get_one_or_create(db, model, create_method='',
+                      create_method_kwargs=None,
+                      **kwargs):
+    one = get_one(db, model, **kwargs)
+    if one:
+        return one, False
+    else:
+        __transaction = db.begin_nested()
+        try:
+            if 'on_multiple' in kwargs:
+                # This kwarg is supported by get_one() but not by create().
+                del kwargs['on_multiple']
+            obj = create(db, model, create_method, create_method_kwargs, **kwargs)
+            __transaction.commit()
+            return obj
+        except IntegrityError, e:
+            logging.info(
+                "INTEGRITY ERROR on %r %r, %r: %r", model, create_method_kwargs, 
+                kwargs, e)
+            __transaction.rollback()
+            return db.query(model).filter_by(**kwargs).one(), False
+
+
+def create(db, model, create_method='',
+           create_method_kwargs=None,
+           **kwargs):
+    kwargs.update(create_method_kwargs or {})
+    created = getattr(model, create_method, model)(**kwargs)
+    db.add(created)
+    db.flush()
+    return created, True
+
+
+
+class Configuration(object):
+    INTEGRATIONS = "integrations"
+    DATA_DIRECTORY = "data_directory"
+    DATABASE_INTEGRATION = "Postgres"
+    DATABASE_PRODUCTION_URL = "production_url"
+    DATABASE_TEST_URL = "test_url"
+
+    log = logging.getLogger("Configuration file loader")
+
+    instance = None
+
+    @classmethod
+    def database_url(cls, test=False):
+        if test:
+            key = cls.DATABASE_TEST_URL
+        else:
+            key = cls.DATABASE_PRODUCTION_URL
+        return cls.integration(cls.DATABASE_INTEGRATION)[key]
+
+        
+    @classmethod
+    def data_directory(cls):
+        return cls.get(cls.DATA_DIRECTORY)
+
+
+    @classmethod
+    def get(cls, key, default=None):
+        if not cls.instance:
+            raise ValueError("No configuration file loaded!")
+        return cls.instance.get(key, default)
+
+
+    @classmethod
+    def integration(cls, name, required=False):
+        """Find an integration configuration by name."""
+        integrations = cls.get(cls.INTEGRATIONS, {})
+        v = integrations.get(name, {})
+        if not v and required:
+            raise ValueError(
+                "Required integration '%s' was not defined! I see: %r" % (
+                    name, ", ".join(sorted(integrations.keys()))
+                )
+            )
+        return v
+
+
+    @classmethod
+    def load(cls):
+        #cfv = 'SIMPLIFIED_CONFIGURATION_FILE'
+        #if not cfv in os.environ:
+        #    print "CannotLoadConfiguration: No configuration file defined in %s." % cfv
+
+        config_path = 'config.json'
+        try:
+            cls.log.info("Loading configuration from %s", config_path)
+            configuration = cls._load(open(config_path).read())
+        except Exception, e:
+            print "CannotLoadConfiguration: Error loading configuration file %s: %s" % (
+                    config_path, e)
+            
+        cls.instance = configuration
+        return configuration
+
+
+    @classmethod
+    def _load(cls, str):
+        lines = [x for x in str.split("\n") if not x.strip().startswith("#")]
+        try:
+            result = json.loads("\n".join(lines))
+        except Exception, e:
+            print "CannotLoadConfiguration: Error loading configuration file %s: %s" % (e, lines)
+
+        return result
+
+
+
+class ItemCollection(Base):
+    """ Linker for many-to-many relationship between display items and their 
+    associated collection (curated experiences). """
+    __tablename__ = 'items_collections'
+    id = Column(Integer, primary_key=True)
+    item_id = Column(Integer, ForeignKey('display_items.id'), index=True)
+    collection_id = Column(Integer, ForeignKey('collections.id'), index=True)
+
+    @classmethod
+    def from_collection(cls, collection):
+        item_coll = ItemCollection()
+        item_coll.collection = collection
+        return item_coll
+
+
+
+class Collection(Base):
+    """ A curated experience, involving a bunch of items.  """
+    __tablename__ = 'collections'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    curator = Column(String)
+    items = association_proxy('item_collections', 'display_item')
+    item_collections = relationship("ItemCollection", backref="collection", cascade="all, delete, delete-orphan")
+
+
+
+class DisplayItem(Base):
+    """
+    An item on display, s.a. a book, an item of dress, a photograph, etc..
+    """
+    __tablename__ = 'display_items'
+    id = Column(Integer, primary_key=True)
+    beacon_major_id = Column(Integer, index=True)
+    beacon_minor_id = Column(Integer, index=True)
+
+    title = Column(Unicode, index=True)
+    cover_image = Column(Unicode)
+
+    # an Item can have any number of downloads or streams associated with it.
+    media_resources = relationship("MediaResource", backref="display_item")
+    # an Item belongs to a curated experience
+    collections = association_proxy('item_collections', 'collection', creator=ItemCollection.from_collection)
+    item_collections = relationship("ItemCollection", backref="display_item", cascade="all, delete, delete-orphan")
+
+
+
+class MediaResource(Base):
+    """
+    A pdf, epub, jpeg, streamed movie, etc..
+    """
+    __tablename__ = 'media_resources'
+    id = Column(Integer, primary_key=True)
+    display_item_id = Column(Integer, ForeignKey('display_items.id'), index=True, nullable=False)
+
+
+
+class FulfillmentInfo(object):
+    """A record of an attempt to download a media object. """
+
+    def __init__(self, identifier, content_link, content_type):
+        """      
+        :param identifier Item's unique id.
+        :param content_link Either URL to download ACSM file from or URL to streaming content.
+        :param content_type Media type of the book version we're getting.  
+        """
+
+        self.identifier = identifier
+        self.content_link = content_link
+        self.content_type = content_type
+    
+
+
+class SessionManager(object):
+
+    # Materialized views need to be created and indexed from SQL
+    # commands kept in files. This dictionary maps the views to the
+    # SQL files.
+
+    engine_for_url = {}
+
+    @classmethod
+    def engine(cls, url=None):
+        url = url or Configuration.database_url()
+        return create_engine(url, echo=False)
+
+    @classmethod
+    def sessionmaker(cls, url=None):
+        engine = cls.engine(url)
+        return sessionmaker(bind=engine)
+
+    @classmethod
+    def initialize(cls, url):
+        if url in cls.engine_for_url:
+            engine = cls.engine_for_url[url]
+            return engine, engine.connect()
+
+        engine = cls.engine(url)
+        Base.metadata.create_all(engine)
+
+        base_path = os.path.split(__file__)[0]
+        resource_path = os.path.join(base_path, "files")
+
+        connection = None
+
+        if not connection:
+            connection = engine.connect()
+
+        if connection:
+            connection.close()
+
+        cls.engine_for_url[url] = engine
+        return engine, engine.connect()
+
+    @classmethod
+    def refresh_materialized_views(self, _db):
+        pass
+
+    @classmethod
+    def session(cls, url):
+        engine = connection = 0
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=sa_exc.SAWarning)
+            engine, connection = cls.initialize(url)
+        session = Session(connection)
+        cls.initialize_data(session)
+        session.commit()
+        return session
+
+    @classmethod
+    def initialize_data(cls, session):
+        pass
+
+
+        
+
+
+
+
+
+
+
---
+++ tests/test_model.py
@@ -0,0 +1,242 @@
+from datetime import (
+    datetime,
+    timedelta,
+)
+
+from nose.tools import (
+    eq_, 
+    assert_raises,
+    assert_raises_regexp,
+    set_trace,
+)
+
+from model import (
+    Collection, 
+    Configuration, 
+    DisplayItem,
+    FulfillmentInfo, 
+    get_one_or_create,
+    ItemCollection, 
+    MediaResource, 
+    production_session,
+    SessionManager, 
+    temp_config, 
+)
+
+import logging 
+import mock
+import model
+import os
+import shutil
+import tempfile
+#os.environ['TESTING'] = 'true'
+
+from sqlalchemy.orm.session import Session
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Configuration.load()
+Base = declarative_base()
+
+
+def package_setup():
+    """Make sure the database schema is initialized and initial
+    data is in place.
+    """
+    engine, connection = DatabaseTest.get_database_connection()
+
+    # First, recreate the schema.
+    #
+    # Base.metadata.drop_all(connection) doesn't work here, so we
+    # approximate by dropping everything except the materialized
+    # views.
+    for table in reversed(Base.metadata.sorted_tables):
+        if not table.name.startswith('mv_'):
+            engine.execute(table.delete())
+
+    Base.metadata.create_all(connection)
+
+    # Initialize basic database data needed by the application.
+    _db = Session(connection)
+    SessionManager.initialize_data(_db)
+    _db.commit()
+    connection.close()
+    engine.dispose()
+
+
+class DatabaseTest(object):
+
+    engine = None
+    connection = None
+
+    @classmethod
+    def get_database_connection(cls):
+        url = Configuration.database_url(test=True)
+        engine, connection = SessionManager.initialize(url)
+
+        return engine, connection
+
+    @classmethod
+    def setup_class(cls):
+        # Initialize a temporary data directory.
+        cls.engine, cls.connection = cls.get_database_connection()
+        cls.old_data_dir = Configuration.data_directory
+        cls.tmp_data_dir = tempfile.mkdtemp(dir="/tmp")
+        Configuration.instance[Configuration.DATA_DIRECTORY] = cls.tmp_data_dir
+
+        os.environ['TESTING'] = 'true'
+
+    @classmethod
+    def teardown_class(cls):
+        # Destroy the database connection and engine.
+        cls.connection.close()
+        cls.engine.dispose()
+
+        if cls.tmp_data_dir.startswith("/tmp"):
+            logging.debug("Removing temporary directory %s" % cls.tmp_data_dir)
+            shutil.rmtree(cls.tmp_data_dir)
+        else:
+            logging.warn("Cowardly refusing to remove 'temporary' directory %s" % cls.tmp_data_dir)
+
+        Configuration.instance[Configuration.DATA_DIRECTORY] = cls.old_data_dir
+        if 'TESTING' in os.environ:
+            del os.environ['TESTING']
+
+    def setup(self):
+        # Create a new connection to the database.
+        self._db = Session(self.connection)
+        self.transaction = self.connection.begin_nested()
+
+        # Start with a high number so it won't interfere with tests that search for an age or grade
+        self.counter = 2000
+
+        self.time_counter = datetime(2014, 1, 1)
+        self.isbns = ["9780674368279", "0636920028468", "9781936460236"]
+        #self.search_mock = mock.patch(model.__name__ + ".ExternalSearchIndex", DummyExternalSearchIndex)
+        #self.search_mock.start()
+
+        # TODO:  keeping this for now, but need to fix it bc it hits _isbn, 
+        # which pops an isbn off the list and messes tests up.  so exclude 
+        # _ functions from participating.
+        # also attempt to stop nosetest showing docstrings instead of function names.
+        #for name, obj in inspect.getmembers(self):
+        #    if inspect.isfunction(obj) and obj.__name__.startswith('test_'):
+        #        obj.__doc__ = None
+
+
+    def teardown(self):
+        # Close the session.
+        self._db.close()
+
+        # Roll back all database changes that happened during this
+        # test, whether in the session that was just closed or some
+        # other session.
+        self.transaction.rollback()
+        #self.search_mock.stop()
+
+
+    @property
+    def _id(self):
+        self.counter += 1
+        return self.counter
+
+
+
+class DummyHTTPClient(object):
+
+    def __init__(self):
+        self.responses = []
+        self.requests = []
+
+    def queue_response(self, response_code, media_type="text/html",
+                       other_headers=None, content=''):
+        headers = {}
+        if media_type:
+            headers["content-type"] = media_type
+        if other_headers:
+            for k, v in other_headers.items():
+                headers[k.lower()] = v
+        self.responses.append((response_code, headers, content))
+
+    def do_get(self, url, headers, **kwargs):
+        self.requests.append(url)
+        return self.responses.pop()
+
+
+
+class MockRequestsResponse(object):
+    """A mock object that simulates an HTTP response from the
+    `requests` library.
+    """
+    def __init__(self, status_code, headers={}, content=None, url=None):
+        self.status_code = status_code
+        self.headers = headers
+        self.content = content
+        self.url = url or "http://url/"
+
+    def json(self):
+        return json.loads(self.content)
+
+    @property
+    def text(self):
+        return self.content.decode("utf8")
+
+
+
+class DisplayItemTest(DatabaseTest):
+
+    def setup(self):
+        super(DisplayItemTest, self).setup()
+
+        #self.api = MockOneClickAPI.from_config(self._db)
+        #base_path = os.path.split(__file__)[0]
+        #self.resource_path = os.path.join(base_path, "files", "oneclick")
+
+        #self.default_patron = self._patron(external_identifier="oneclick_testuser")
+        #self.default_patron.authorization_identifier="13057226"
+
+    '''
+    def get_data(self, filename):
+        # returns contents of sample file as string and as dict
+        path = os.path.join(self.resource_path, filename)
+        data = open(path).read()
+        return data, json.loads(data)
+    '''
+
+
+    def test_create_item(self):
+        #datastr, datadict = self.get_data("response_patron_internal_id_not_found.json")
+        #self.api.queue_response(status_code=200, content=datastr)
+        #oneclick_patron_id = self.api.get_patron_internal_id(patron_cardno='9305722621')
+
+        beet_beacon_major_id = '65370'
+        beet_beacon_minor_id = '49339'
+        beet_beacon_title = 'Beet Beacon'
+
+        lemon_beacon_major_id = '48448'
+        lemon_beacon_minor_id = '18544'
+        lemon_beacon_title = 'Lemon Tart Beacon'
+
+        item, made_new = get_one_or_create(
+            self._db, DisplayItem, create_method_kwargs=dict(
+                beacon_major_id=beet_beacon_major_id,
+                beacon_minor_id=beet_beacon_minor_id,
+                ), id=self._id
+        )
+        eq_(True, made_new)
+
+        collection, made_new = get_one_or_create(
+            self._db, Collection, create_method_kwargs=dict(
+                name="Cool Books",
+                curator="Jane Curator",
+                ), id=self._id
+        )
+        eq_(True, made_new)
+
+        item.collections.append(collection)
+        eq_(collection.id, item.collections[0].id)
+
+
+
+
+
